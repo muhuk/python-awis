muhuk/python-awis

commit 984cdf7df21890a42ea92d70fadce51778c00e26

Fixed bug retrieving sites with tilde character.

awis/__init__.py
@@ -103,6 +103,7 @@ class AwisApi(object):
         )
 
     def url_info(self, urls, *response_groups, **kwargs):
+        urls = map(urllib.quote, urls)
         params = { "Action": "UrlInfo" }
         if not isinstance(urls, (list, tuple)):
             params.update({

commit 307a0f14cb26089bedb65a418a71953a35a9a144

fix single Url queries (bug introduced by commit 984cdf7d)

awis/__init__.py
@@ -103,22 +103,21 @@ class AwisApi(object):
         )
 
     def url_info(self, urls, *response_groups, **kwargs):
-        urls = map(urllib.quote, urls)
         params = { "Action": "UrlInfo" }
         if not isinstance(urls, (list, tuple)):
             params.update({
-                "Url": urls,
+                "Url": urllib.quote(urls),
                 "ResponseGroup": ",".join(response_groups),
              })
         else:
             if len(urls) > 5:
                 raise RuntimeError, "Maximum number of batch URLs is 5."
 
-            params.update({ "UrlInfo.Shared.ResponseGroup": ",".join(response_groups), })    
-            
+            params.update({ "UrlInfo.Shared.ResponseGroup": ",".join(response_groups), })
+
             for i, url in enumerate(urls):
-                params.update({"UrlInfo.%d.Url" % (i + 1): url})
-        
+                params.update({"UrlInfo.%d.Url" % (i + 1): urllib.quote(url)})
+
         return self.request(params, **kwargs)
 
     @staticmethod